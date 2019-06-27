f88632f06e714f25802506c3da60e7217f42bc19
Jatin Kumar <jatinkumarg17@gmail.com>
2019-06-27 16:41:09 -0400

    Updated navbar

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -5,9 +5,35 @@ export default class Navbar extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link to="/" className="navbar-brand">ExcerTracker</Link>
-        <div className="collpase navbar-collapse">
+      <nav class="navbar sticky-top navbar-expand-lg navbar-light" style={{backgroundColor: 'transparent!important', Height:'60px', fontSize : '20px'}}>
+  <a class="navbar-brand" href="/" style={{Height:'60px', fontSize : '26px', fontWeight:"bold" }}>Trackerr</a>
+  <div class=" navbar-collapse" id="navbarNav" style={{ paddingLeft: "10px"}}>
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="/">Home </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/">Exercises</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/create">Create Exercise Log</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/user">Create User</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+    );
+  }
+}
+
+
+//rough
+/*
+<nav className="navbar navbar-dark bg-primary" style={{Height:'60px', fontSize : '16px'}}>
+        <Link to="/" className="navbar-brand">Trackerr</Link>
+        <div className="collpase navbar-collapse" style={{paddingRight: '0',paddingLeft: '0', textAlign: 'right'}}>
         <ul className="navbar-nav mr-auto">
           <li className="navbar-item">
           <Link to="/" className="nav-link">Exercises</Link>
@@ -21,6 +47,4 @@ export default class Navbar extends Component {
         </ul>
         </div>
       </nav>
-    );
-  }
-}
+      */
