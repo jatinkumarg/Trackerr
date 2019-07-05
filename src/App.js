ec3db1dfe2b8b16bef83e0cac9034e1c42af94ea
Jatin Kumar <jatinkumarg17@gmail.com>
2019-07-04 23:35:23 -0400

    Added not page found

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import "./App.css";
 
 import Navbar from "./components/navbar.component"
@@ -9,17 +9,21 @@ import EditExercise from "./components/edit-exercise.component";
 import CreateExercise from "./components/create-exercise.component";
 import CreateUser from "./components/create-user.component";
 import Home from "./components/home.component";
+import NotFound from "./components/notfound.component";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <div className="container">
+      <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/list" exact component={ExercisesList} />
       <Route path="/edit/:id" component={EditExercise} />
       <Route path="/create" component={CreateExercise} />
       <Route path="/user" component={CreateUser} />
+      <Route path="*" component={NotFound} />
+      </Switch>
       </div>
     </Router>
   );
